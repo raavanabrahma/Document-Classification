4dcb30d1bcd0156248d7958884d1d9c3bb91c301
RAJANIKANTH KAKANI <rajnikanthkakani@gmail.com>
2023-03-26 12:14:14 +0530

    Intitial

---
+++ dcrm/mydb.py
@@ -0,0 +1,10 @@
+import mysql.connector
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password ='12345678'
+
+)
+cursorObject = dataBase.cursor()
+cursorObject.execute("CREATE DATABASE testtoaster")
+print("done")
